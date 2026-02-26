e415c4b2890727252c74b0c1bb8d9df9fe0f8c05
Dmitry631 <rodenko.dmytro@chnu.edu.ua>
2026-02-26 13:27:31 +0200

    ADD: news page

---
+++ news.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php'; ?>
+
+<h2>Latest news</h2>
+
+<div class="news-item">
+    <h3>First news (news.php)</h3>
+    <p>News for a demonstration of work</p>
+</div>
+
+<div class="news-item">
+    <h3>Second news</h3>
+    <p>template news</p>
+</div>
+
+<?php include 'includes/sidebar.php'; ?>
+<?php include 'includes/footer.php'; ?>
