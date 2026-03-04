fb718ed325166c62df2e78722c7d95d840e80016
Dmitry631 <rodenko.dmytro@chnu.edu.ua>
2026-03-04 20:50:31 +0200

    ADD: news page

---
+++ views/news.php
@@ -0,0 +1,44 @@
+<?php include '../layout/header.php'; ?>
+
+<h2>Latest news</h2>
+
+<div class="news-item">
+    <div class="news-preview">
+        <img src="../img/news3.jpg" alt="S26 Ultra">
+    </div>
+
+    <div class="news-text">
+        <h3>Samsung S26 Ultra</h3>
+        <p>Samsung's new flagship, the Galaxy S26 Ultra, does not disappoint.</p>
+        <span class="date"> February 25, 2026</span>
+
+    </div>
+</div>
+
+<div class="news-item">
+    <div class="news-preview">
+        <img src="../img/news1.jpg" alt="Iphone 17e">
+    </div>
+
+    <div class="news-text">
+        <h3>Iphone 17e</h3>
+        <p>Apple today announced the new iPhone 17e</p>
+        <span class="date"> March 03, 2026</span>
+    </div>
+
+</div>
+
+<div class="news-item">
+    <div class="news-preview">
+        <img src="../img/news2.jpg" alt="MacBook Neo">
+    </div>
+
+    <div class="news-text">
+        <h3>New MacBook NEO</h3>
+        <p>Apple's new MacBook Neo gives the company its cheapest laptop ever, creating a budget entry into the Mac lineup</p>
+        <span class="date"> March 04, 2026</span>
+    </div>
+</div>
+
+<?php include '../layout/sidebar.php'; ?>
+<?php include '../layout/footer.php'; ?>
