f837b7266b5d922c488418eca2fecc8ba1c7c0c8
Dmitry631 <rodenko.dmytro@chnu.edu.ua>
2026-03-29 13:40:38 +0300

    style: add news category

--- views/news.php
+++ views/news.php
@@ -6,10 +6,11 @@
     </div>
 
     <div class="news-text">
+        <span class="news-category">Smartphones</span>
+
         <h3>Samsung S26 Ultra</h3>
         <p>Samsung's new flagship, the Galaxy S26 Ultra, does not disappoint.</p>
-        <span class="date"> February 25, 2026</span>
-
+        <span class="news-date"> February 25, 2026</span>
     </div>
 </div>
 
@@ -17,11 +18,13 @@
     <div class="news-preview">
         <img src="./img/news1.jpg" alt="Iphone 17e">
     </div>
+    <span class="news-category">Smartphones</span>
 
     <div class="news-text">
+
         <h3>Iphone 17e</h3>
         <p>Apple today announced the new iPhone 17e</p>
-        <span class="date"> March 03, 2026</span>
+        <span class="news-date"> March 03, 2026</span>
     </div>
 
 </div>
@@ -31,9 +34,12 @@
         <img src="./img/news2.jpg" alt="MacBook Neo">
     </div>
 
+    <span class="news-category">Laptops</span>
+
     <div class="news-text">
+
         <h3>New MacBook NEO</h3>
         <p>Apple's new MacBook Neo gives the company its cheapest laptop ever, creating a budget entry into the Mac lineup</p>
-        <span class="date"> March 04, 2026</span>
+        <span class="news-date"> March 04, 2026</span>
     </div>
 </div>
